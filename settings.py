93687ba0b2707f36e9864e354f7c190d939abdf3
oliverhuangchao <chaoh@g.clemson.edu>
2018-07-30 23:18:33 -0700

    use system environment

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
-WORKING_DIR = "/Users/chaoh/Documents/yanghet/flask-demo"
+WORKING_DIR = os.environ.get("FLASK_WORKING_DIR")
 DB_PATH = os.path.join(WORKING_DIR, "var/data/main.db")
 USER_TABLE = "users"
 TEMPLATE_DIR = os.path.join(WORKING_DIR, "templates")
